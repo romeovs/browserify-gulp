4df8dde07af20cb95febcdcc69b34315c1d92743
Romeo Van Snick <romeovs@gmail.com>
2015-06-23 18:46:01 +0200

    fix scope

--- src/defaults/log-time.js
+++ src/defaults/log-time.js
@@ -3,9 +3,10 @@ import gutil from 'gulp-util'
 var bs = gutil.colors.cyan('browserify');
 
 export default function(time) {
+  let info = '';
   if ( time ) {
     var tm = gutil.colors.magenta(`${time}ms`);
-    const info = ` after ${tm}`;
+    info = ` after ${tm}`;
   }
   gutil.log(`Finished ${bs} build${info}.`);
 };
