b5119d71f0e39426ef7ad2c763c1f7e49feea80a
Romeo Van Snick <romeovs@gmail.com>
2015-12-19 13:41:01 +0100

    use gulp-util to log error

--- src/defaults/log-error.js
+++ src/defaults/log-error.js
@@ -1,8 +1,6 @@
 import gutil from 'gulp-util'
 
 export default function(err) {
-  // log error
-  var error = new gutil.PluginError('Browserify', err);
+  const error = new gutil.PluginError('Browserify', err);
   gutil.log(error);
-
 };
